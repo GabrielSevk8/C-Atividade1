08c7b33f7b52fd84b1f684816e748664558f88e8
GabrielSevk8 <135348439+GabrielSevk8@users.noreply.github.com>
2023-11-01 11:34:26 -0300

    C# atividade.1 - 01/11/2023

---
+++ CursoCSharp/Fundamentos/FormatandoNumeros.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    class FormatandoNumeros {
+
+        public static void Executar() {
+            //Arredondar numero
+            double valor = 15.175;
+            Console.WriteLine(valor.ToString("F1"));
+
+            //Transformar um valor monetário (moeda de acordo com idioma da máquina)
+            Console.WriteLine(valor.ToString("C"));
+
+            //multiplicar um valor por 100 e adiciona percentual
+            Console.WriteLine(valor.ToString("P"));
+
+            //Formatando o número do jeito que você quiser
+            Console.WriteLine(valor.ToString("#.##"));
+        }
+    }
+}
---
+++ CursoCSharp/Fundamentos/OperadoresAritmeticos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos {
+    class OperadoresAritmeticos {
+
+        public static void Executar() {
+
+            /*
+            Console.WriteLine("Insira o numero para a soma: ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira outro numero: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Resultado: {n1+n2}");
+            */
+
+            Console.WriteLine("Escreva a Altura: ");
+            double A = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escreva a Base: ");
+            double B = double.Parse(Console.ReadLine());
+
+            double resultado = B * A / 2;
+
+            Console.WriteLine($"A area é igual a {resultado.ToString("#.#")}");
+        }
+    }
+}
